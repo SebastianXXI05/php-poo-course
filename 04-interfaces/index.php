28f52db581b35ed21299e6da20c728e9f095d965
Sebastian <Sebastian>
2023-03-24 21:12:57 -0500

    interfaces finished

---
+++ 04-interfaces/index.php
@@ -0,0 +1,40 @@
+<?php
+/* 
+Una interfaz define las estructura que una clase debe tener, es decir los metodos que
+tiene que tener la clase. La implementación de estos metodos no son concretas.
+*/
+
+interface Db {
+  public function connection();
+}
+
+interface Models extends Db { // heredando metodos de otra interfaz
+  public function insert();
+  public function get_all();
+  public function update();
+  public function remove();
+}
+
+interface a extends Db, Models { // herencia multiple
+  public function a();
+}
+
+class User implements Models {
+  public function connection() {
+    
+  }
+  public function insert() {
+
+  }
+  public function get_all() {
+
+  }
+  public function update() {
+
+  }
+  public function remove() {
+
+  }
+
+  // si no defino estos metodos me da error.
+}
