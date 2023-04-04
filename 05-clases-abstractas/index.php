7f6238596eeb1a2f93276581348e4abc9188ace5
Sebastian <Sebastian005X>
2023-04-04 13:49:10 -0500

    add abstract class

---
+++ 05-clases-abstractas/index.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+Una clase abstracta es una clase que no puede ser instanciada directamente, sino que debe ser heredada por otras clases. Las clases abstractas se utilizan para definir un comportamiento común que deben implementar las clases hijas, pero que no puede ser completamente definido en la clase abstracta.
+ */
+
+abstract class ClaseAbstracta {
+  abstract protected function getValor();
+  abstract protected function valorPrejifo($prejifo);
+  abstract public function printName($name);
+
+  public function imprimir() {
+    echo $this->getValor();
+  }
+}
+
+// Esto no se puede
+//$objeto = new ClaseAbstracta;
+
+
+class ClaseConcreta extends ClaseAbstracta {
+  protected function getValor() {
+    return 'Clase concreta'; 
+  }
+  protected function valorPrejifo($prejifo) {
+    return $prejifo.' Clase concreta';
+  }
+  public function printName($name) {
+   echo $name; 
+  }
+}
+
+$clase1 = new ClaseConcreta;
+
+$clase1->imprimir();
+
+
